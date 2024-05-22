4ddddd63a505b612137ce1b8f9ba5f6474079e1a
Rikinyan <karasutengu0319@gmail.com>
2024-05-23 03:31:07 +0900

    fix

--- zshz.sh
+++ zshz.sh
@@ -1,3 +1,5 @@
+#!/bin/zsh
+
 file_data=history
 
 line2path() {
@@ -13,21 +15,25 @@ line2access_time() {
 }
 
 add_or_update_history() {
+    setopt LOCAL_OPTIONS SH_WORD_SPLIT
+
     added_path=$1
     new_access_time=$2
-    file_lines=$(cat $file_data | tr "\n" " ")
+    file_lines=$(cat $file_data)
 
     new_updated_file_date=""
     for line in $file_lines
     do
         path=$(line2path "$line")
         access_time=$(line2access_time "$line")
+        echo $path $access_time
+        if [ -z $path ] || [ -z $access_time ] && continue
 
         if [ "$path" != "$added_path" ]
         then
             new_updated_file_date="${new_updated_file_date}${path}|${access_time}\n"
         fi
-    done       
+    done
 
     new_updated_file_date="${new_updated_file_date}${added_path}|${new_access_time}\n"
 
@@ -35,12 +41,13 @@ add_or_update_history() {
 }
 
 jump() {
-    [ $# = 0 ] && (cd || exit)
+    setopt LOCAL_OPTIONS SH_WORD_SPLIT
+    [ $# != 1 ] && (cd || exit)
 
     search_pattern="$1"
 
     file_data=history
-    file_lines=$(cat $file_data | tr "\n" " ")
+    file_lines=$(cat $file_data)
 
     # get cd's candidate
     best_candidate=0
@@ -70,9 +77,14 @@ jump() {
     fi
 }
 
+add_or_pwd_to_history() {
+    echo "update file"
+    add_or_update_history $(pwd) $(date +%s)
+}
+
 ## main scripts
-#autoload -Uz add-zsh-hook
-#add-zsh-hook chpwd add_or_update_history
+autoload -Uz add-zsh-hook
+add-zsh-hook chpwd add_or_pwd_to_history
 
 # parse options
 while getopts :l opt 2> /dev/null
